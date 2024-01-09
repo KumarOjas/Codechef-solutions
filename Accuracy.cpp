3378d3b05356c13821388222b49ec26f44010e22
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2024-01-08 22:46:51 -0800

    Create Accuracy.cpp

---
+++ Accuracy.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main (){
+
+  long t;
+  
+  scanf ("%ld", &t);
+  
+  while(t--)
+    {
+    
+        long x; 
+        
+        scanf("%ld", &x);
+        
+       printf("%ld\n", (3 - x % 3) % 3);
+    }   
+
+}
