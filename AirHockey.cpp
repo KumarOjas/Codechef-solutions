c4b1b435befdfd80fabd9d71299946b17f6d9636
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2024-01-09 20:59:41 -0800

    Create AirHockey.cpp

---
+++ AirHockey.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main (){
+
+  long t;
+  scanf ("%ld", &t);
+
+   while(t--){
+        long a, b; 
+     
+        scanf("%ld %ld", &a, &b);
+        
+       printf("%ld\n", 7 - (a > b ? a : b));
+    }   
+
+}
