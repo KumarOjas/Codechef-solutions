dec6813fd3339e9d0a364b84cf14df2853d5350b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2024-01-09 20:50:09 -0800

    Create Advance.cpp

---
+++ Advance.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main (){
+
+    long t; 
+  
+    scanf("%ld", &t);
+     
+    while(t--){
+     
+        long x, y; 
+      
+        scanf("%ld %ld", &x, &y);
+        
+        puts((x <= y && y <= x + 200) ? "YES" : "NO");
+    }   
+
+}
