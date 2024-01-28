cdcf9089ebfeb9c3eb98d1e0afb4ad58ffb5e84e
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2024-01-28 01:14:29 -0800

    Create Watercons.cpp

---
+++ Watercons.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main (){
+
+   long t ;
+  
+  
+  scanf ("%ld", &t);
+  
+  while(t--)
+     {
+        long x; 
+       
+         scanf("%ld", &x);
+        
+         puts(x >= 2000 ? "YES" : "NO");
+    }
+
+}
