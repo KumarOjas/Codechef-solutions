ca5bfa22b513806499cf94580b5633ce7489408b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2024-02-01 03:25:05 -0800

    Create Volcontrol.cpp

---
+++ Volcontrol.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main (){
+long t ;
+
+  scanf("%ld",&t);
+
+   while(t--){
+
+  
+     long x,y;
+    
+     
+     
+     scanf("%ld %ld", &x, &y);
+     
+     printf("%ld\n", (x < y) ? (y - x) : (x - y));
+    }   
+
+}
